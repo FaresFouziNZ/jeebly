f347c84ec7de23d95918b53877c256a1e9a692ca
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2022-07-24 17:28:34 +0300

    add Food class

---
+++ jebaa/lib/Classes/food.dart
@@ -0,0 +1,22 @@
+class Food {
+  String name;
+  int price;
+  int quantity = 0;
+  Food({this.name, this.price, this.quantity});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'price': price,
+      'quantity': quantity,
+    };
+  }
+
+  factory Food.fromMap(Map<String, dynamic> map) {
+    return Food(
+      name: map['name'],
+      price: map['price'],
+      quantity: map['quantity'],
+    );
+  }
+}
